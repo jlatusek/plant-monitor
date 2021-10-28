cb321a5e1bb3dda28804f16300efa4ff89a8240d
Jakub Latusek <jk.latusek@gmail.com>
2021-10-28 17:27:38 +0200

    Add database interaction function
    Added function
    * GetPlant by Id
    * Get all plants

--- controllers/plant.go
+++ controllers/plant.go
@@ -1,11 +1,22 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"plant_monitor/database"
+	"plant_monitor/models"
 )
 
 func GetPlant(c *fiber.Ctx) error {
-	msg := fmt.Sprintf("Plant id: %s", c.Params("id"))
-	return c.SendString(msg)
+	plant, err := models.GetPlantById(database.MI, c.Params("id"))
+	if err != nil {
+		return c.SendString(err.Error())
+	}
+	return c.JSON(plant)
+}
+func GetPlantAll(c *fiber.Ctx) error {
+	plants, err := models.GetPlantAll(database.MI)
+	if err != nil {
+		return c.SendString(err.Error())
+	}
+	return c.JSON(plants)
 }
