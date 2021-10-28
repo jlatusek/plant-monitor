e214e19e3cdaa41280e879d832e26b1753759a35
Jakub Latusek <jk.latusek@gmail.com>
2021-10-28 14:02:45 +0200

    Add configuration load from .env or system variable

---
+++ configuration/config.go
@@ -0,0 +1,43 @@
+package configuration
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"reflect"
+	"strconv"
+)
+
+type Configuration struct {
+	RootUsername string
+	RootPassword string
+	MongoUri     string
+	DbName       string
+	Port         int
+}
+
+var ServerConfiguration Configuration
+
+func (conf *Configuration) Load() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	conf.MongoUri = os.Getenv("MONGO_URI")
+	conf.DbName = os.Getenv("DB_NAME")
+	conf.Port, err = strconv.Atoi(os.Getenv("PORT"))
+	conf.RootUsername = os.Getenv("DB_ROOT_USERNAME")
+	conf.RootPassword = os.Getenv("DB_ROOT_PASSWORD")
+	if err != nil {
+		log.Fatal("Error loading .env file, wrong PORT data type")
+	}
+}
+
+func (conf Configuration) Print() {
+	v := reflect.ValueOf(conf)
+	typeOf := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		fmt.Printf("Field: %-10s \t Value: %v\n", typeOf.Field(i).Name, v.Field(i).Interface())
+	}
+}
